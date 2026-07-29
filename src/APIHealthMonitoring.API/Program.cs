e7a3220d36f760970eee7be7dc76260f676b8a9d
Mohamed Alaa <mohamedalaa2556@gmail.com>
2026-07-29 23:03:02 +0300

    unneccessary

--- src/APIHealthMonitoring.API/Program.cs
+++ src/APIHealthMonitoring.API/Program.cs
@@ -69,7 +69,7 @@ namespace APIHealthMonitoring
                     var securityScheme = new OpenApiSecurityScheme
                     {
                         Name         = "Authorization",
-                        Description  = "Enter:  {your JWT token}",
+                        Description  = "Enter: Bearer {your JWT token}",
                         In           = ParameterLocation.Header,
                         Type         = SecuritySchemeType.ApiKey,
                         Scheme       = "Bearer",
